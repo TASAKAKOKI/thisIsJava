0e2c7f68253cdda01359405cf8e416499a64869c
TASAKA KOKI <agiks21@gmail.com>
2020-12-29 00:16:24 +0900

    chap15_Set컬렉션

---
+++ chap15_컬렉션_프레임워크/src/sec03_Set컬렉션/exam01_/Member.java
@@ -0,0 +1,25 @@
+package sec03_SetÄÃ·º¼Ç.exam01_;
+
+public class Member {
+	public String name;
+	public int age;
+	
+	public Member(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if(obj instanceof Member) {
+			Member member = (Member) obj;
+			return member.name.equals(name) && member.age == age;
+		}
+		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		return name.hashCode() + age;
+	}
+}
