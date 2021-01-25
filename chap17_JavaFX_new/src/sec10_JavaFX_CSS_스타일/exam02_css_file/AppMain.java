7cf789bba8224c92e013d0c86ac6de01301db23f
TASAKA KOKI <agiks21@gmail.com>
2021-01-25 13:40:12 +0900

    chap17_JavaFX_CSS_style

---
+++ chap17_JavaFX_new/src/sec10_JavaFX_CSS_스타일/exam02_css_file/AppMain.java
@@ -0,0 +1,24 @@
+package sec10_JavaFX_CSS_스타일.exam02_css_file;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class AppMain extends Application{
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent parent = FXMLLoader.load(getClass().getResource("root.fxml"));
+		Scene scene = new Scene(parent);
+		//scene에서 css스타일을 적용
+		scene.getStylesheets().add(getClass().getResource("app.css").toString());
+		
+		primaryStage.setTitle("Inline Style");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
